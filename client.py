f9a0712d3156f9402b32fb7cc2d0136a34a8cb5a
Марина Титова <39424031+Marina-Titova@users.noreply.github.com>
2018-12-28 20:45:22 +0700

    Add files via upload

---
+++ client.py
@@ -0,0 +1,63 @@
+import socket
+import time
+
+class ClientError(Exception):
+
+    pass
+
+class Client(object):
+
+    def __init__(self, host, port, timeout=None):
+        self.host = host
+        self.port = port
+        self.timeout = timeout
+
+    def put(self, key, value, timestamp=None):
+        timestamp = timestamp or str(int(time.time()))
+
+        try:
+            with socket.create_connection((self.host, self.port)) as sock:
+                msg = 'put {} {} {}\n'.format(key, value, timestamp)
+                sock.sendall(msg.encode('utf8'))
+                acpt = sock.recv(1024).decode('utf8')
+
+                if acpt == 'ok\n\n':
+                    return {}
+
+                if acpt == 'error\nwrong command\n\n':
+                    raise ClientError
+
+        except socket.error:
+            raise ClientError
+
+    def get(self, metric):
+        try:
+            with socket.create_connection((self.host, self.port)) as sock:
+                sock.sendall('get {}\n'.encode('utf8').format(metric))
+                acpt = sock.recv(1024).decode('utf8')
+
+                if acpt == 'ok\n\n':
+                    return {}
+
+                if acpt == 'error\nwrong command\n\n':
+                    raise ClientError
+
+                msg_list = acpt[3:].split('\n')
+                metrics = dict()
+                for i in range(len(msg_list) - 2):
+                    msg_list[i] = msg_list[i].split(' ')
+                    if msg_list[i][0] in metrics.keys():
+                        n = 0
+                        for tpl in metrics[msg_list[i][0]]:
+                            if tpl[0] > int(msg_list[i][2]):
+                                metrics[msg_list[i][0]].insert(n, tuple(int(msg_list[i][2]), float(msg_list[i][1])))
+                            else:
+                                n += 1
+                    else:
+                        metrics[msg_list[i][0]] = [(int(msg_list[i][2]), float(msg_list[i][1]))]
+
+                return metrics
+
+        except socket.error:
+            raise ClientError
+
